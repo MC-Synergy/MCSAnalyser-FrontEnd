3ab3f1db390907fa9e0b7175131db1c85b12b201
Vermeire,Vincent V.L <v.vermeire@student.fontys.nl>
2022-10-06 15:52:46 +0200

    added beginning of static charts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./View/Pages/Layout"
 import StaticGraphs from "./View/Pages/StaticGraphs"
 import CustomGraphs from "./View/Pages/CustomGraphs"
 import NoPage from "./View/Pages/NoPage";
+import { NewStaticGraphs } from "./View/Pages/newStaticGraphs";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
             <Route path="/static-graphs" element={<StaticGraphs />}/>
             <Route path="/custom-graphs" element={<CustomGraphs />}/>
+            <Route path="/beta-static-graphs" element={<NewStaticGraphs />} />
             <Route path="/" element={<Navigate replace={true} to="/static-graphs" />} />
             <Route path="*" element={<NoPage />} />
         </Route>
---
+++ src/View/Pages/newStaticGraphs.tsx
@@ -0,0 +1,10 @@
+import { Graph } from "../Components/Graph";
+
+export function NewStaticGraphs(){
+    return (
+        <>
+        <div>hallo</div>
+        <Graph/>
+        </>
+    )
+}
