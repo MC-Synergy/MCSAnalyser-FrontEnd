08bc7ceabb85d07231fc517133443acac178e4a5
crazyvinvin <vvermeire55@gmail.com>
2022-10-12 15:57:39 +0200

    now only one dynamic production graph component

---
+++ src/View/Components/Graphs/Production/ProductionGraph.tsx
@@ -0,0 +1,83 @@
+import Chart from 'chart.js/auto';
+import {ChartConfiguration} from "chart.js";
+import { useEffect } from "react";
+import { createProductionGraphDataSets, useProductionData } from "../../../../Services/productionService";
+import 'chartjs-adapter-moment';
+import { BorderColors, DataSet } from "../../../../Models/Types";
+import "../../../Styles/GraphStyles.css"
+
+export interface ProductionGraphProps {
+    canvasID: string,
+    lineColors: BorderColors,
+    mcsSystemID: number,
+    accumulated: boolean
+}
+
+export function ProductionGraph({canvasID, lineColors, mcsSystemID, accumulated}: ProductionGraphProps){
+
+    const {data: mcsSystem, loading} = useProductionData(mcsSystemID, accumulated);
+    
+    useEffect(() => {
+        if (loading) {
+            return
+        }
+
+        const datasets: DataSet[] = createProductionGraphDataSets(mcsSystem.itemsProduced, lineColors)
+        const chartConfig : ChartConfiguration = {
+            type: 'line',
+            data: {
+                datasets: datasets
+            },
+            options: {
+                parsing: {
+                    xAxisKey: 'timeSent',
+                    yAxisKey: 'amountProduced'
+                },
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            displayFormats: {
+                                hour: 'HH'
+                            }
+                        },
+                        grid: {
+                            color: "#aacfd1"
+                        }
+                    },
+                    y: {
+                        grid: {
+                            color: "#aacfd1"
+                        }
+                    }
+                },
+                plugins: {
+                    legend: { 
+                        position: 'top'
+                    },
+                    title: {
+                        display: true,
+                        text: mcsSystem.name,
+                        position: 'top',
+                    }
+                },
+
+            }
+        }
+        const canvas = document.getElementById(canvasID) as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+        const myChart = new Chart(ctx, chartConfig)
+
+        return () => {
+            myChart.destroy()
+        }
+        
+    }, [loading, mcsSystem])
+
+    return (
+        <div className="production-graph">
+            <canvas id={canvasID}></canvas>
+        </div>
+    )
+}
--- src/View/Pages/StaticGraphs.tsx
+++ src/View/Pages/StaticGraphs.tsx
@@ -1,15 +1,65 @@
-import { useProductionData } from "../../Services/productionService";
+import { ProductionGraph, ProductionGraphProps } from "../Components/Graphs/Production/ProductionGraph";
+
+
+// export interface ProductionGraphProps {
+//     canvasID: string,
+//     lineColors: BorderColors,
+//     mcsSystemID: number,
+//     accumulated: boolean
+// }
 
 function StaticGraphs() {
+  const MelonPumpkinAccumulatedProps: ProductionGraphProps = {
+    canvasID: "MelonPumpkinAccumulatedGraph",
+    lineColors: {
+      "minecraft:melon": "rgba(133, 139, 28, 1)",
+      "minecraft:pumpkin": "rgba(220, 136, 30, 1)"
+    },
+    mcsSystemID: 10,
+    accumulated: true
+  }
 
-  const {data, loading} = useProductionData(10)
+  const MelonPumpkinProps: ProductionGraphProps = {
+    canvasID: "MelonPumpkinGraph",
+    lineColors: {
+      "minecraft:melon": "rgba(133, 139, 28, 1)",
+      "minecraft:pumpkin": "rgba(220, 136, 30, 1)"
+    },
+    mcsSystemID: 10,
+    accumulated: false
+  }
+
+  const WoolAccumulatedProps: ProductionGraphProps = {
+    canvasID: "WoolGraphAccumulated",
+    lineColors: {
+      "minecraft:white_wool": "rgba(247, 250, 250, 1)",
+      "minecraft:orange_wool": "rgba(254, 125, 20, 1)",
+      "minecraft:magenta_wool": "rgba(200, 72, 190, 1)",
+      "minecraft:light_blue_wool": "rgba(61, 186, 230, 1)",
+      "minecraft:yellow_wool": "rgba(255, 209, 41, 1)",
+      "minecraft:lime_wool": "rgba(119, 196, 26, 1)",
+      "minecraft:pink_wool": "rgba(251, 149, 182, 1)",
+      "minecraft:grey_wool": "rgba(66, 72, 75, 1)",
+      "minecraft:light_grey_wool": "rgba(151, 151, 142, 1)",
+      "minecraft:cyan_wool": "rgba(22, 145, 154, 1)",
+      "minecraft:purple_wool": "rgba(128, 45, 182, 1)",
+      "minecraft:blue_wool": "rgba(56, 60, 166, 1)",
+      "minecraft:brown_wool": "rgba(121, 75, 42, 1)",
+      "minecraft:green_wool": "rgba(89, 116, 29, 1)",
+      "minecraft:red_wool": "rgba(170, 41, 36, 1)",
+      "minecraft:black_wool": "rgba(21, 22, 26, 1)"
+    },
+    mcsSystemID: 15,
+    accumulated: true
+  }
 
   return (
-    <div>
-      {loading && <div>Fetching data...</div>}
-      {!loading && <div>This is it: {data[0].itemName}</div>}
+    <div id="staticGraphs">
+      <ProductionGraph {...MelonPumpkinAccumulatedProps}></ProductionGraph>
+      <ProductionGraph {...MelonPumpkinProps}></ProductionGraph>
+      <ProductionGraph {...WoolAccumulatedProps}></ProductionGraph>
     </div>
-  );
+  )
 }
 
 export default StaticGraphs;
