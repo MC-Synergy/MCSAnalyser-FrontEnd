669ebd55d7a615065c873934190ef6fa813e08ba
crazyvinvin <vvermeire55@gmail.com>
2022-10-12 15:57:56 +0200

    set graph color and update some imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './View/Styles/index.css';
 import App from './App'
+import { Chart } from 'chart.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+Chart.defaults.color = "#aacfd1";
+
 root.render(
   <React.StrictMode>
     <App />
