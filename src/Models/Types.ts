8e9c9b6bc9ace07ed5c4a99bbbe576f3c6432a2a
crazyvinvin <vvermeire55@gmail.com>
2022-10-12 15:58:04 +0200

    added types

---
+++ src/Models/Types.ts
@@ -0,0 +1,33 @@
+export interface ProductionPoint {
+    id: number,
+    mcsSystemID: number,
+    timeSent: Date,
+    amountProduced: number,
+    timeSpanAsSeconds: number,
+    itemName: string
+}
+
+export interface ItemsProduced {
+    [key: string]: ProductionPoint[]
+}
+
+export interface MCSSystem {
+    id: number,
+    name: string,
+    itemsProduced: ItemsProduced
+}
+
+export interface DataSet {
+    label: string,
+    data: any[],
+    parsing: {
+        xAxisKey: string,
+        yAxisKey: string
+    }
+    borderColor: string;
+}
+
+export interface BorderColors {
+    //itemName : borderColor
+    [itemName: string]: string 
+}
