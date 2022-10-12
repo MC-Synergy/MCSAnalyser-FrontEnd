d809fbe22ac1c9c9723717d116b587819301413f
crazyvinvin <vvermeire55@gmail.com>
2022-10-12 16:24:02 +0200

    added non accumulated wool graph

--- src/View/Pages/StaticGraphs.tsx
+++ src/View/Pages/StaticGraphs.tsx
@@ -53,11 +53,36 @@ function StaticGraphs() {
     accumulated: true
   }
 
+  const WoolProps: ProductionGraphProps = {
+    canvasID: "WoolGraphAccumulated",
+    lineColors: {
+      "minecraft:white_wool": "rgba(247, 250, 250, 1)",
+      "minecraft:orange_wool": "rgba(254, 125, 20, 1)",
+      "minecraft:magenta_wool": "rgba(200, 72, 190, 1)",
+      "minecraft:light_blue_wool": "rgba(61, 186, 230, 1)",
+      "minecraft:yellow_wool": "rgba(255, 209, 41, 1)",
+      "minecraft:lime_wool": "rgba(119, 196, 26, 1)",
+      "minecraft:pink_wool": "rgba(251, 149, 182, 1)",
+      "minecraft:grey_wool": "rgba(66, 72, 75, 1)",
+      "minecraft:light_grey_wool": "rgba(151, 151, 142, 1)",
+      "minecraft:cyan_wool": "rgba(22, 145, 154, 1)",
+      "minecraft:purple_wool": "rgba(128, 45, 182, 1)",
+      "minecraft:blue_wool": "rgba(56, 60, 166, 1)",
+      "minecraft:brown_wool": "rgba(121, 75, 42, 1)",
+      "minecraft:green_wool": "rgba(89, 116, 29, 1)",
+      "minecraft:red_wool": "rgba(170, 41, 36, 1)",
+      "minecraft:black_wool": "rgba(21, 22, 26, 1)"
+    },
+    mcsSystemID: 15,
+    accumulated: false
+  }
+
   return (
     <div id="staticGraphs">
       <ProductionGraph {...MelonPumpkinAccumulatedProps}></ProductionGraph>
       <ProductionGraph {...MelonPumpkinProps}></ProductionGraph>
       <ProductionGraph {...WoolAccumulatedProps}></ProductionGraph>
+      <ProductionGraph {...WoolProps}></ProductionGraph>
     </div>
   )
 }
