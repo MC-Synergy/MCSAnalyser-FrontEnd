feff17150a7b95199b3620ff306fab96af597432
crazyvinvin <vvermeire55@gmail.com>
2022-10-12 15:58:22 +0200

    moved types to types.ts file

--- src/Models/DataPoints.ts
+++
@@ -1,8 +0,0 @@
-export interface ProductionPoint {
-    id: number,
-    mcsSystemID: number,
-    timeSent: Date,
-    amountProduced: number,
-    timeSpanAsSeconds: number,
-    itemName: String
-}
