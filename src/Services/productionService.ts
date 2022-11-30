ac9a9c9ce3980b71e54561824699c13dc1963cb9
crazyvinvin <vvermeire55@gmail.com>
2022-11-30 17:33:48 +0100

    fixed last commit

--- src/Services/productionService.ts
+++ src/Services/productionService.ts
@@ -37,37 +37,41 @@ export function useProductionData(mcsSystemID: number, accumulated: boolean, tim
   const [loading, setLoading] = useState(true);
 
   const refreshData = useCallback(async () => {
-      try {
-        setLoading(true);
-        let params: any = {
-          mcssystemid: mcsSystemID,
-          accumulated: accumulated,
-        }
-
-        if (timeSpanAsMinutes !== undefined) {
-          params["datatimespan"] = timeSpanAsMinutes
-        }
-
-        if (intervalAsMinutes !== undefined) {
-          params["interval"] = intervalAsMinutes
-        }
-        else {
-          params["interval"] = 0
-        }
-
-        const { data: mcsaResponse } = await axios.get(BASE_URL + '/get-by-system-id', {params: params})
-        const { data: systemDataResponse } = await axios.get(SYSTEMS_API_URL + '/get-system', {params: {SystemID: mcsSystemID}}) 
-        setData({
-          id: mcsSystemID,
-          name: systemDataResponse.Name,
-          itemsProduced: mcsaResponse
-        } as MCSSystem);
-        
-      } catch (err) {
-        console.error(err)
+    let errorThrown = false
+    try {
+      setLoading(true);
+      let params: any = {
+        mcssystemid: mcsSystemID,
+        accumulated: accumulated,
       }
-    setLoading(false);
-    }, [mcsSystemID, accumulated, timeSpanAsMinutes, intervalAsMinutes]);
+
+      if (timeSpanAsMinutes !== undefined) {
+        params["datatimespan"] = timeSpanAsMinutes
+      }
+
+      if (intervalAsMinutes !== undefined) {
+        params["interval"] = intervalAsMinutes
+      }
+      else {
+        params["interval"] = 0
+      }
+
+      const { data: mcsaResponse } = await axios.get(BASE_URL + '/get-by-system-id', {params: params})
+      const { data: systemDataResponse } = await axios.get(SYSTEMS_API_URL + '/get-system', {params: {SystemID: mcsSystemID}}) 
+      setData({
+        id: mcsSystemID,
+        name: systemDataResponse.Name,
+        itemsProduced: mcsaResponse
+      } as MCSSystem);
+      
+    } catch (err) {
+      errorThrown = true
+      console.error(err)
+    }
+    if (!errorThrown) {
+      setLoading(false);
+    }
+  }, [mcsSystemID, accumulated, timeSpanAsMinutes, intervalAsMinutes]);
 
   return {
     data,
