15d56d6039a64f8ed8f84163f58141aa0bae1c0a
crazyvinvin <vvermeire55@gmail.com>
2022-12-02 14:53:20 +0100

    lhci token test

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
     upload: {
       target: 'lhci',
       serverBaseUrl: 'https:lighthouse.mcsynergy.nl',
-      token: LHCI_TOKEN,
+      token: '99089327-fb55-4aba-ba07-b40af2ded973',
     },
     assert: {
       preset: 'lighthouse:recommended',
