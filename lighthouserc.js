eb271f74c55e2dbdf9d2091eaebb7a3ed8588c82
crazyvinvin <vvermeire55@gmail.com>
2022-12-04 12:39:55 +0100

    lhci test

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     upload: {
       target: 'lhci',
-      serverBaseUrl: 'https:lighthouse.mcsynergy.nl',
+      serverBaseUrl: 'https://lighthouse.mcsynergy.nl',
       token: '99089327-fb55-4aba-ba07-b40af2ded973',
     },
     assert: {
